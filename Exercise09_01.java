2743aef7b634eb50009a1ab03b7d8da1e4a927ff
LibertyPerry <77160908+LibertyPerry@users.noreply.github.com>
2021-01-27 09:37:06 -0700

    Update Exercise09_01.java

--- Exercise09_01.java
+++ Exercise09_01.java
@@ -2,11 +2,11 @@
 public class Exercise09_01 {
 	public static void main(String[] args) {
 		Rectangle rectangle1 = new Rectangle(4, 40);
-		System.out.println("The Area of a rectange with " + rectangle1.width + " width and " + rectangle1.height + " height is " + rectangle1.getArea());
+		System.out.println("The Area of a rectangle with " + rectangle1.width + " width and " + rectangle1.height + " height is " + rectangle1.getArea());
 		System.out.println("The Perimeter of this rectangle is " + rectangle1.getPerimeter());
 		
 		Rectangle rectangle2 = new Rectangle(3.5, 35.9);
-		System.out.println("The Area of a rectange with " + rectangle2.width + " width and " + rectangle2.height + " height is " + rectangle2.getArea());
+		System.out.println("The Area of a rectangle with " + rectangle2.width + " width and " + rectangle2.height + " height is " + rectangle2.getArea());
 		System.out.println("The Perimeter of this rectangle is " + rectangle2.getPerimeter());
 	}
 
